f3357f701194c87d73f58bf9d445e47c4cbe0c2a
Yagrr <90156764+Yagrr@users.noreply.github.com>
2026-06-26 13:20:52 +0100

    feat(cli): add basic CLI prompt and modify config to include 'node' types

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import { createInterface } from "node:readline";
+
+export function startREPL(): void {
+    const repl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+    repl.prompt();
+    repl.on("line", (input): void => {
+        if (input === "") {
+            repl.prompt();
+        } else {
+            const inputClean = cleanInput(input);
+            console.log(`Your command was: ${inputClean[0]}`);
+            repl.prompt();
+        } 
+    });
+}
+
+
+
 export function cleanInput(input: string): string[] {
     return input.trim().toLowerCase().split(" ");
 }
